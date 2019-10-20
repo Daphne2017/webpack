ece34858265dbf03cb164badf77a5dc3630e99f8
yanyanshan <2190907815@qq.com>
2019-10-20 22:00:07 +0800

    feat：添加笔记

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,11 @@ const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
-    entry: './src/js/index.js',  
+    entry: './src/js/index.js',  // 打包文件入口
     output: {
-        path: path.resolve(__dirname, 'public'),  // 打包输出的目录名称是什么
-        filename: 'js/bundle-[hash].js'  // 配置哈希随机文件名,打包到js目录下,如果文件没有修改，则随机的哈希名字没有改变
+        path: path.resolve(__dirname, 'public'),  // 打包输出的目录名称是什么，打包出口目录
+        filename: 'js/bundle-[hash].js'  // 配置哈希随机文件名,打包到js目录下,如果文件没有修改
+        // 则随机的哈希名字没有改变
     },
   // 代理配置地址：https://segmentfault.com/a/1190000016199721
   // 代理配置地址：https://www.cnblogs.com/beka/p/8479710.html
@@ -37,7 +38,8 @@ module.exports = {
                 },
                 exclude:/node_modules/
             },
-            {
+            // css提取配置信息
+            { 
                 test:/\.css$/,
                 // use: ['style-loader','css-loader'],
                 use:ExtractTextPlugin.extract({
@@ -64,10 +66,10 @@ module.exports = {
         ]
     },
     plugins:[
-      new ExtractTextPlugin('./css/[name].css'),
+      new ExtractTextPlugin('./css/[name].css'), // 输出css文件，打包css文件
 	    new HtmlWebpackPlugin({
           template:"./src/index.html",
-        //   filename:'a.html',
+        //   filename:'a.html',  // 生成的文件名，不写默认打包生成输出口目录下的index.html,
 	      minify:{
 	          removeAttributeQuotes:true,//去除引号
 	          removeComments:true,//去除注释
